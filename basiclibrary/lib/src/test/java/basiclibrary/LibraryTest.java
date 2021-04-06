4232b0f8ae0d737380fe3c376704cb1ee13c89b7
alkhateeb49 <mohamadalkhateeb49@yahoo.com>
2021-04-06 16:19:58 +0300

    lab03

--- basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
@@ -5,7 +5,9 @@ package basiclibrary;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -41,6 +43,35 @@ public class LibraryTest {
         int [] test =weeklyMonthTemperatures[2];
         int [] result = Library.arrAvg(weeklyMonthTemperatures);
         assertEquals("Test should equal:", test, result);
+    }
+
+    @Test public void testWeather(){
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        String data=Library.Weather(weeklyMonthTemperatures);
+        assertEquals("The String is : ","High: 72\n" + "Low: 51\n" + "Never saw temperature: 63\n" + "Never saw temperature: 67\n" + "Never " +
+                "saw temperature: 68\n" + "Never saw temperature: 69\n", data);
+    }
+
+
+    @Test public void testTally(){
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+        String winner = Library.tally(votes);
+        assertEquals("Winner is: ", "Bush", Library.tally(votes));
     }
 
 
